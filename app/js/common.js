c6512beb13a7793502be60ecdd478c4c0ff784ec
alemesh <alemesh@acceptic.com>
2018-06-21 15:09:29 +0300

    added 2 pages and starting tabs

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
 	// Custom JS
+    (function($) {
+        $(function() {
+
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('ul.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            });
+
+        });
+    })(jQuery);
+    $(".menu-lisy").click(function() {
+        // $(".tabs__caption").slideToggle();
+        $(".menu-lisy").toggleClass('menu-active');
+    });
 
 });
