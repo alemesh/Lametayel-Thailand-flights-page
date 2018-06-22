09cf32967b000482628be26d1897f0bff9a30dd2
alemesh <alemesh@acceptic.com>
2018-06-22 17:49:49 +0300

    finish mobile version

--- app/js/common.js
+++ app/js/common.js
@@ -9,6 +9,11 @@ $(function() {
                     .addClass('active').siblings().removeClass('active')
                     .closest('div.tabs').find('ul.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
             });
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            });
 
         });
     })(jQuery);
@@ -17,4 +22,90 @@ $(function() {
         $(".menu-lisy").toggleClass('menu-active');
     });
 
+    initSlickCarousel();
+
+
+
+
+    $('#mybtn').on('click', function(e) {
+        /* your code here */
+        e.preventDefault();
+        var cssValues = {
+            "height":"100%",
+            "transition:": ".5s ease"
+        };
+        var cssValues2 = {
+            "transition:": ".5s ease",
+            "height":"0"
+        };
+        var myattr = $(this).attr("data-btn");
+        console.log(myattr);
+        if(myattr === 'btnopen'){
+            $(this).attr("data-btn", 'btnclose');
+            $(".second-block").css(cssValues);
+        }else {
+            $(this).attr("data-btn", 'btnopen');
+            $(".second-block").css(cssValues2);
+        }
+
+        // $(".second-block").css(cssValues);
+
+
+    });
+
+
+
+
+
+
+
 });
+
+
+function initSlickCarousel() {
+    jQuery('.slick-slider').slick({
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        // infinite: true,
+        // adaptiveHeight: true,
+        rows: 0,
+        // prevArrow: '<span class="slick-prev"></span>',
+        // nextArrow: '<span class="slick-next"></span>',
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                // centerMode: true,
+                autoplay: true,
+                // speed: 2000,
+                slidesToShow: 3
+            }
+        },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                    // centerMode: true,
+                    autoplay: true,
+                    // speed: 2000,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                    // centerMode: true,
+                    autoplay: true,
+                    // speed: 2000,
+                    slidesToShow: 1
+                }
+            }]
+    });
+}
