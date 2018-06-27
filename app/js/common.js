09568616204da8cbf4208f0b398b9ed3fc623940
alemesh <alemesh@acceptic.com>
2018-06-27 19:16:43 +0300

    fixes css

--- app/js/common.js
+++ app/js/common.js
@@ -14,6 +14,11 @@ $(function() {
                     .addClass('active').siblings().removeClass('active')
                     .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
             });
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('h3.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            });
 
         });
     })(jQuery);
