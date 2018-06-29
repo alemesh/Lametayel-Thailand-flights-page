2d63df8665cb3e47c4eeca5a90535f2e2857ee7d
alemesh <alemesh@acceptic.com>
2018-06-29 18:20:06 +0300

    fixt js bugg

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,7 @@ $(function() {
 
 	// Custom JS
     (function($) {
-        $(function() {
+        // $(function() {
 
             $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
                 $(this)
@@ -20,7 +20,7 @@ $(function() {
                     .closest('div.tabs').find('h3.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
             });
 
-        });
+        // });
     })(jQuery);
     $(".menu-lisy").click(function() {
         // $(".tabs__caption").slideToggle();
